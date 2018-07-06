1ed33668ad9d2abd243a2d4620bf100e2179272e
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-07-05 23:02:22 -0500

    insurrance app

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your age?");
+            byte age = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Do you have any DUIs? true or false");
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets?");
+            byte speeding = byte.Parse(Console.ReadLine());
+            person driver = new person(age, dui, speeding);
+            driver.CanGetInsurance();
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleApp5/person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class person
+    {
+        private byte age { get; set; }
+        private bool dui { get; set; }
+        private byte speeding { get; set; }
+        public person(byte age, bool dui, byte speeding)
+        {
+            this.age = age;
+            this.dui = dui;
+            this.speeding = speeding;
+        }
+        public void CanGetInsurance()
+        {
+            bool insure = age > 15 && dui == false && speeding < 3;
+            Console.WriteLine("Qualified\n{0}", insure);
+        }
+    }
+}
